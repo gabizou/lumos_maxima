3dc8f1c75510e36c586b7d0ceb935a68ebb4206a
Gabriel Harris-Rouquette <github@gabizou.com>
2021-04-03 22:50:58 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+import org.spongepowered.gradle.plugin.config.PluginLoaders
+import org.spongepowered.plugin.metadata.PluginDependency
+
+plugins {
+    `java-library`
+    id("org.spongepowered.gradle.plugin") version "1.0.2"
+}
+
+group = "org.spongepowered"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+sponge {
+    apiVersion("8.0.0")
+    plugin("example") {
+        loader(PluginLoaders.JAVA_PLAIN)
+        displayName("Example")
+        mainClass("org.spongepowered.example.Example")
+        description("Just testing things...")
+        links {
+            homepage("https://spongepowered.org")
+            source("https://spongepowered.org/source")
+            issues("https://spongepowered.org/issues")
+        }
+        contributor("Spongie") {
+            description("Lead Developer")
+        }
+        dependency("spongeapi") {
+            loadOrder(PluginDependency.LoadOrder.AFTER)
+            optional(false)
+        }
+    }
+}
+
+val javaTarget = 8 // Sponge targets a minimum of Java 8
+java {
+    sourceCompatibility = JavaVersion.toVersion(javaTarget)
+    targetCompatibility = JavaVersion.toVersion(javaTarget)
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+    options.apply {
+        encoding = "utf-8" // Consistent source file encoding
+        if (JavaVersion.current().isJava10Compatible) {
+            release.set(javaTarget)
+        }
+    }
+}
+
+// Make sure all tasks which produce archives (jar, sources jar, javadoc jar, etc) produce more consistent output
+tasks.withType(AbstractArchiveTask::class).configureEach {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
+}
